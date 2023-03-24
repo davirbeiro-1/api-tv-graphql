c322cb768d93e1a2c9c5d21e1c8700fdf390da39
davirbeiro <davirbeiro1@hotmail.com>
2023-03-24 19:22:21 +0000

    feat: add relationship between tv shows and actors

--- database.ts
+++ database.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize-typescript';
-import { Actor } from './src/dto/model/actor-model';
-import { Episode } from './src/dto/model/episode-model';
-import { TvShow } from './src/dto/model/tv-show-model';
+import { Actor } from './src/dto/model/actor.model';
+import { Episode } from './src/dto/model/episode.model';
+import { TvShowActor } from './src/dto/model/tv-show-actor.model';
+import { TvShow } from './src/dto/model/tv-show.model';
 import { User } from './src/dto/model/user-model';
 
 export const sequelize = new Sequelize({
@@ -9,5 +10,5 @@ export const sequelize = new Sequelize({
   username: 'root',
   password: '123456',
   dialect: 'mysql',
-  models: [User, TvShow, Episode, Actor],
+  models: [User, TvShow, Episode, Actor, TvShowActor],
 });
