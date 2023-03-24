47bd1a6529f6a02c17588c955445a618420ab766
davirbeiro <davirbeiro1@hotmail.com>
2023-03-24 14:45:15 +0000

    feat: create sequelize connection

---
+++ database.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Actor } from './src/dto/model/actor-model';
+import { Episode } from './src/dto/model/episode-model';
+import { TvShow } from './src/dto/model/tv-show-model';
+import { User } from './src/dto/model/user-model';
+
+export const sequelize = new Sequelize({
+  database: 'tekever',
+  username: 'root',
+  password: '123456',
+  dialect: 'mysql',
+  models: [User, TvShow, Episode, Actor],
+});
