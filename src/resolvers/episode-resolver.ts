1c654b3d055c82e9ad340ee234feba82a9ff1415
davirbeiro <davirbeiro1@hotmail.com>
2023-03-24 14:49:18 +0000

    feat: create episode resolver

---
+++ src/resolvers/episode-resolver.ts
@@ -0,0 +1,34 @@
+import { Arg, Mutation, Resolver, Query, Ctx } from 'type-graphql';
+import { Episode } from '../dto/model/episode-model';
+
+@Resolver()
+export class EpisodeResolver {
+    @Query(() => String)
+    async episode() {
+        return 'Teste'
+    }
+
+    @Mutation(() => Episode)
+    async createEpisode(
+        @Arg('name') name: string,
+        @Arg('isReleased') isReleased: boolean,
+        @Arg('releaseDate') releaseDate: string,
+        @Arg('duration') duration: string,
+        @Arg('description') description: string,
+    ): Promise<Episode> {
+
+
+        const episode = new Episode({
+            name,          
+            isReleased,
+            releaseDate,          
+            duration,
+            description,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
+        await episode.save();
+
+        return episode;
+    }
+}
