d422daef55df7ea89bcb870cc9d0d667a18f7d2e
davirbeiro <davirbeiro1@hotmail.com>
2023-03-25 12:23:45 +0000

    chore: undo modifications

--- src/resolvers/episode-resolver.ts
+++ src/resolvers/episode-resolver.ts
@@ -11,15 +11,25 @@ export class EpisodeResolver {
 
     @Mutation(() => Episode)
     async createEpisode(
-        @Arg('episodeInput') episodeInput: CreateEpisodeInput
+        @Arg('name') name: string,
+        @Arg('isReleased') isReleased: boolean,
+        @Arg('releaseDate') releaseDate: string,
+        @Arg('duration') duration: string,
+        @Arg('tvShowId') tvShowId: number,
+        @Arg('description') description: string,
     ): Promise<Episode> {
 
         const episode = new Episode({
-            ...episodeInput,
+            name,
+            isReleased,
+            releaseDate,
+            duration,
+            description,
+            tvShowId,
             createdAt: new Date(),
             updatedAt: new Date()
         });
-        
+
         await episode.save();
 
         return episode;
