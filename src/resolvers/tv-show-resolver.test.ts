8dce52cb98b45fdc13b8efd3015955ff36b947b0
davirbeiro <davirbeiro1@hotmail.com>
2023-03-25 21:15:28 +0000

    test: add test case to tvshows queries

--- src/resolvers/tv-show-resolver.test.ts
+++ src/resolvers/tv-show-resolver.test.ts
@@ -18,6 +18,21 @@ mutation CreateTvShow($actorsIds: [String!]!, $description: String!, $numberOfSe
     createTvShow(actorsIds: $actorsIds, description: $description, numberOfSeasons: $numberOfSeasons, numberOfEpisodes: $numberOfEpisodes, genre: $genre, endsAt: $endsAt, startsAt: $startsAt, name: $name) {
       name,
       description
+      id
+    }
+  }
+`
+
+const getAllTvShowsQuery = `query GetTvShows {
+    getTvShows {
+      name
+    }
+  }
+`
+
+const getTvByGenreQuery = `query GetTvByGenre($genre: String!) {
+    getTvByGenre(genre: $genre) {
+      name
     }
   }
 `
@@ -52,4 +67,106 @@ describe('Create', () => {
             }
         })
     })
+})
+
+describe('Queries', () => {
+    
+    it("get all tvShows", async () => {
+        await insertActor();
+        const tvShow = {
+            actorsIds: [
+                "1"
+            ],
+            description: faker.datatype.string(6),
+            numberOfSeasons: faker.datatype.float().toString(),
+            numberOfEpisodes: faker.datatype.float().toString(),
+            genre: "action",
+            endsAt: faker.date.recent().toString(),
+            startsAt: faker.date.recent().toString(),
+            name: faker.name.firstName()
+        }
+        const response = await graphQlCall({
+            source: tvShowMutation,
+            variableValues: tvShow
+
+        })
+
+        const response2 = await graphQlCall({
+            source: getAllTvShowsQuery,
+            variableValues: {}
+        })
+
+        expect(response2.data?.getTvShows.length).toBeGreaterThan(0)
+    })
+
+    it("get tv by genre", async () => {
+        await insertActor();
+        const tvShow = {
+            actorsIds: [
+                "1"
+            ],
+            description: faker.datatype.string(6),
+            numberOfSeasons: faker.datatype.float().toString(),
+            numberOfEpisodes: faker.datatype.float().toString(),
+            genre: "action",
+            endsAt: faker.date.recent().toString(),
+            startsAt: faker.date.recent().toString(),
+            name: faker.name.firstName()
+        }
+        
+        await graphQlCall({
+            source: tvShowMutation,
+            variableValues: tvShow
+
+        })
+
+        const genre = {
+            genre: 'action'
+        }
+
+        const response2 = await graphQlCall({
+            source: getTvByGenreQuery,
+            variableValues: genre
+        })
+
+        expect(response2.data?.getTvByGenre.length).toBeGreaterThan(0)
+    })
+
+const getTvShowsByActorId = `
+query Query($actorId: Float!) {
+    getTvShowsByActorId(actorId: $actorId)
+  }
+`
+    it("get actors by tv show id", async () => {
+        await insertActor();
+        const tvShow = {
+            actorsIds: [
+                "1"
+            ],
+            description: faker.datatype.string(6),
+            numberOfSeasons: faker.datatype.float().toString(),
+            numberOfEpisodes: faker.datatype.float().toString(),
+            genre: "action",
+            endsAt: faker.date.recent().toString(),
+            startsAt: faker.date.recent().toString(),
+            name: faker.name.firstName()
+        }
+        
+        const response = await graphQlCall({
+            source: tvShowMutation,
+            variableValues: tvShow
+
+        })
+        console.log(response.data?.createTvShow.id)
+        const teste = {
+           actorId: Number("1")
+        }
+
+        const response2 = await graphQlCall({
+            source: getTvShowsByActorId,
+            variableValues: teste
+        })
+        expect(response2.data?.getTvShowsByActorId.length).toBeGreaterThan(0)
+    })
+
 })
