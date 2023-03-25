3fd45992d66d67e34f1617b15a6edbc66c0c1f8f
davirbeiro <davirbeiro1@hotmail.com>
2023-03-25 12:23:19 +0000

    feat: add query that brings all tvshows that an actor appears

--- src/resolvers/actor-resolver.ts
+++ src/resolvers/actor-resolver.ts
@@ -1,11 +1,21 @@
-import { Arg, Mutation, Resolver, Query, Ctx } from 'type-graphql';
+import { Arg, Mutation, Resolver, Query } from 'type-graphql';
 import { Actor } from '../dto/model/actor.model';
+import { TvShowActor } from '../dto/model/tv-show-actor.model';
+import { TvShow } from '../dto/model/tv-show.model';
 
 @Resolver()
 export class ActorResolver {
-    @Query(() => String)
-    async getActor() {
-        return 'Teste'
+    @Query(() => [String])
+    async getTvShowsByActorId(
+        @Arg('actorId') actorId: number,
+    ) {
+        const actor = await Actor.findByPk(actorId);
+        if (!actor) {
+          throw new Error(`Actor with id ${actorId} not found`);
+        }
+        
+        const tvShows = await actor.$get('tvShows');
+        return tvShows.map(tvShow => tvShow.name);
     }
 
     @Mutation(() => Actor)
@@ -13,7 +23,6 @@ export class ActorResolver {
         @Arg('name') name: string,
         @Arg('age') age: string,
     ): Promise<Actor> {
-
 
         const actor = new Actor({
             name,
