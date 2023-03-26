c819a1f7df6e577758baf04a1091e2a14a7675e3
davirbeiro <davirbeiro1@hotmail.com>
2023-03-26 14:20:36 +0100

    refactor: create models file to help in imports

---
+++ src/utils/models.ts
@@ -0,0 +1,8 @@
+import { Actor } from "../dto/model/actor.model";
+import { Episode } from "../dto/model/episode.model";
+import { TvShowActor } from "../dto/model/tv-show-actor.model";
+import { TvShow } from "../dto/model/tv-show.model";
+import { User } from "../dto/model/user-model";
+import { UserTvShow } from "../dto/model/user-tv-show.model";
+
+export const models = [User, TvShow, Episode, Actor, TvShowActor, UserTvShow]
