95575a1ed65a6cf3065ed4319ef182de53be3187
davirbeiro <davirbeiro1@hotmail.com>
2023-03-26 17:10:02 +0100

    chore: remove unused vars

--- src/types/context.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express';
-import { Sequelize } from 'sequelize-typescript';
-import { User } from '../dto/model/user-model';
-
-export interface Context {
-  db: Sequelize;
-  req: Request;
-  user: User
-}
