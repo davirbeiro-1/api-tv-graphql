27bb1287adc8c886d704dceb54aac401ef8cb722
davirbeiro <davirbeiro1@hotmail.com>
2023-03-26 14:20:58 +0100

    refactor: apply DRY on createSchema.

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -1,12 +1,7 @@
 import { buildSchema } from "type-graphql"
-import { ActorResolver } from "../resolvers/actor-resolver"
-import { AuthResolver } from "../resolvers/auth-resolver"
-import { EpisodeResolver } from "../resolvers/episode-resolver"
-import { TvShowResolver } from "../resolvers/tv-show-resolver"
-import { UserResolver } from "../resolvers/user-resolver"
 
+import {getResolvers} from './resolvers'
 export async function createSchema() {
-    return await buildSchema({
-        resolvers: [UserResolver, AuthResolver, TvShowResolver, EpisodeResolver, ActorResolver]
-    })
+    const resolvers = getResolvers()
+    return await buildSchema({ resolvers})
 }
