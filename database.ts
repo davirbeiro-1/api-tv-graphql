18442f4019712e298c29770628cc99bda63eb165
davirbeiro <davirbeiro1@hotmail.com>
2023-03-26 14:14:02 +0100

    refactor: refactor models imports to a single file

--- database.ts
+++ database.ts
@@ -1,14 +1,10 @@
 import { Sequelize } from 'sequelize-typescript';
-import { Actor } from './src/dto/model/actor.model';
-import { Episode } from './src/dto/model/episode.model';
-import { TvShowActor } from './src/dto/model/tv-show-actor.model';
-import { TvShow } from './src/dto/model/tv-show.model';
-import { User } from './src/dto/model/user-model';
+import { models } from './src/utils/models';
 
 export const sequelize = new Sequelize({
   database: 'tekever',
   username: 'root',
   password: '123456',
   dialect: 'mysql',
-  models: [User, TvShow, Episode, Actor, TvShowActor],
+  models
 });
