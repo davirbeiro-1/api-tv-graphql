04c5f8074952388bc8c32c4f563516507ba41b95
davirbeiro <davirbeiro1@hotmail.com>
2023-03-26 17:09:06 +0100

    test: adjust get tv by genre query

--- src/test-utils/mockQueries.ts
+++ src/test-utils/mockQueries.ts
@@ -5,11 +5,9 @@ const getAllTvShowsQuery = `query GetTvShows {
   }
 `
 
-const getTvByGenreQuery = `query GetTvByGenre($genre: String!) {
-    getTvByGenre(genre: $genre) {
-      name
-    }
-  }
+const getTvByGenreQuery = `query GetTvShows($orderBy: [String!]!, $genre: String!) {
+  getTvByGenre(orderBy: $orderBy, genre: $genre) { name }
+}
 `
 const getActorsByTvShowIdQuery = `query Query($tvShowId: Float!) {
     getActorsByTvShowId(tvShowId: $tvShowId)
